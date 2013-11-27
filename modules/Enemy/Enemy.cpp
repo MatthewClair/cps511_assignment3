12964b9e52a07b257d4ac466ae9aed8fe5df084e
Matthew Clair <Canada.Rox.Gh@gmail.com>
2013-11-27 13:48:23 -0500

    Improve enemy model

--- modules/Enemy/Enemy.cpp
+++ modules/Enemy/Enemy.cpp
@@ -20,43 +20,23 @@ void Enemy::draw()
 		glRotated(angles.y, 0.0, 1.0, 0.0);
 		glRotated(angles.z, 0.0, 0.0, 1.0);
 
-		Vector3D v[3] = {
-			{ 150.0, 0.0,  0.0 },
-			{ 150.0, 0.0, 50.0 },
-			{   0.0, 0.0, 50.0 },
-		};
-		Quad q(v[0], v[1], v[2]);
-		q.draw();
-
-		Vector3D v2[3] = {
-			{   0.0, 50.0, 0.0 },
-			{ 150.0, 50.0, 0.0 },
-			{ 150.0,  0.0, 0.0 },
-		};
-		Quad q2(v2[0], v2[1], v2[2]);
-		q2.draw();
-
 		glPushMatrix();
-			glTranslated(0.0, 50.0, 0.0);
-			Vector3D v3[3] = {
-				{   0.0, 0.0, 50.0 },
-				{ 150.0, 0.0, 50.0 },
-				{ 150.0, 0.0,  0.0 }
-			};
-			Quad q3(v3[0], v3[1], v3[2]);
-			q3.draw();
+			glScaled(50.0, 50.0, 100.0);
+			glutSolidCube(1);
 		glPopMatrix();
 
 		glPushMatrix();
-			glTranslated(0.0, 0.0, 50.0);
-			Vector3D v4[3] = {
-				{ 150.0,  0.0, 0.0 },
-				{ 150.0, 50.0, 0.0 },
-				{   0.0, 50.0, 0.0 },
-			};
-			Quad q4(v4[0], v4[1], v4[2]);
-			q4.draw();
+			glTranslated(50.0, 0, -40.0);
+			glScaled(50.0, 50.0, 50.0);
+			glutSolidCube(1);
 		glPopMatrix();
+
+		glPushMatrix();
+			glTranslated(-50.0, 0, -40.0);
+			glScaled(50.0, 50.0, 50.0);
+			glutSolidCube(1);
+		glPopMatrix();
+
 	glPopMatrix();
 }
 
