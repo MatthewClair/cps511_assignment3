7d6ce486927f28da60b3647aea4d846a8e27bedf
Brandon Cardoso <puzzlepazuru@gmail.com>
2013-11-28 23:11:50 -0500

    less enemies, more spaced out enemies

--- main.cpp
+++ main.cpp
@@ -42,10 +42,10 @@ int main(int argc, const char *argv[])
 	glutKeyboardFunc(keyDownHandler);
 	glutKeyboardUpFunc(keyUpHandler);
 
-	int n = 10;
+	int n = 6;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			Vector3D origin(200*i-(n-1)*100, 100*j-(n-1)*50, -TheWorld.getRadius()/2);
+			Vector3D origin(400*i-(n-1)*200, 200*j-(n-1)*100, -TheWorld.getRadius()/2);
 			Vector3D angles(0, 0, 0);
 			Enemy e(origin, angles);
 			enemies.push_back(e);
